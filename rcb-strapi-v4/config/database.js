b0446f853e9956cf679d0c7f2f5f05463785e67a
Tilo <tilo@tilo-LIFEBOOK-A532.fritz.box>
2022-09-26 15:55:09 +0200

    mit-cloudinary

--- rcb-strapi-v4/config/database.js
+++ rcb-strapi-v4/config/database.js
@@ -1,11 +1,18 @@
-const path = require('path');
+// path: /srv/strapi/mystrapiapp/config/database.js
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'mysql',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DB_HOST'),
+      port: env.int('DB_PORT'),
+      database: env('DB_NAME'),
+      user: env('DB_USER'),
+      password: env('DB_PASS'),
+//      ssl: {
+//        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+//      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
