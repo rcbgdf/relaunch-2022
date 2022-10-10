a57a8325bc68a09a3fcfb54053014ba8440cfc8d
Tilo Thiele <tilo.thiele@emessages.de>
2022-10-10 21:08:54 +0200

    subdir

--- gatsby/src/components/text-carousell/text-carousell.js
+++ gatsby/src/components/text-carousell/text-carousell.js
@@ -11,7 +11,7 @@ export default function TextCaroussell({ items }) {
       <Carousel pause={true}>
         {items.map(item => (
           <Carousel.Item>
-            <GatsbyImage className="d-block w-100" alt="TMI" image={item.image}/>
+            <GatsbyImage className="d-block w-100" alt="TMI" image={item.image} dir="/relaunch-2022" />
             <Carousel.Caption>
                 <h3>{item.title}</h3>
                 <p>{item.text}</p>
