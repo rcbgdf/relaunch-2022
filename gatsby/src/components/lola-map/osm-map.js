097be5027e714b862cce2b0161f318d499d63179
Tilo Thiele <tilo.thiele@emessages.de>
2022-10-22 23:27:35 +0200

    fix2

--- gatsby/src/components/lola-map/osm-map.js
+++ gatsby/src/components/lola-map/osm-map.js
@@ -13,22 +13,22 @@ const lat1=53.49606890629904
 const lat=53.495425879874865
 const lon=10.205826759338379
 
-const OsmMap = () => {
-  return <div>
-    <iframe width='100%' height={350} frameborder='0' scrolling='no' marginheight='0' marginwidth='0'
-      src={`https://www.openstreetmap.org/export/embed.html?bbox=${lon0},${lat0},${lon1},${lat1}&layer=mapnik&marker=${lat},${lon}`}
-      style={{border: '1px', solid: true, black: true}}>
-    </iframe>
-    <div className='osmMapSuffix'>
-      <a href={`https://www.openstreetmap.org/?lat=${lat}&amp;lon=${lon}&amp;zoom=16&amp;layers=M`} target='_blank'>Gr&#246;&#223;ere Karte anzeigen</a>
-    </div>
-  </div>
-}
 
 const Map = () => {
   const cookieConsent = getCookieConsentValue()
   console.log('cookieConsent='+cookieConsent)
-  if(cookieConsent) return <OsmMap/>
+
+  if(cookieConsent)
+  return <div>
+      <iframe width='100%' height={350} frameborder='0' scrolling='no' marginheight='0' marginwidth='0'
+        src={`https://www.openstreetmap.org/export/embed.html?bbox=${lon0},${lat0},${lon1},${lat1}&layer=mapnik&marker=${lat},${lon}`}
+        style={{border: '1px', solid: true, black: true}}>
+      </iframe>
+      <div className='osmMapSuffix'>
+        <a href={`https://www.openstreetmap.org/?lat=${lat}&amp;lon=${lon}&amp;zoom=16&amp;layers=M`} target='_blank'>Gr&#246;&#223;ere Karte anzeigen</a>
+      </div>
+    </div>
+
   return <div className='staticOsmImage'>
       <StaticImage transformOptions={{fit: "inside"}} alt="Anfahrt Platzhalter" src="../../../static/images/osm-placeholder.png"/>
     </div>
