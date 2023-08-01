b4b5566cb5c6dbe41ffab4e5a375f2b496de9b57
Master Toast <toastmaster@rc-bgdf.de>
2023-08-01 22:37:34 +0200

    pull-skript-funzt

--- rcb-strapi-3.6.6/migrate-strapi-3.6.6-to-4.x/pull-data.sh
+++ rcb-strapi-3.6.6/migrate-strapi-3.6.6-to-4.x/pull-data.sh
@@ -50,7 +50,7 @@ CSV_FILE=`realpath $BASEDIR/$1.csv`
 # --columns="$2" \
 # "$CSV_FILE"
 sudo mysql -uroot $MYSQL_DB_NAME <<EOF
-LOAD DATA INFILE '$CSV_FILE'
+LOAD DATA LOCAL INFILE '$CSV_FILE'
 INTO TABLE $1
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"' LINES
@@ -76,6 +76,80 @@ EOF
 #mysql -ustrapi -pstrapi $MYSQL_DB_NAME <<EOF
 #    alter table clubabende add beitragsbild varchar(24);
 #EOF
+# _id,inhalt,created_at,updated_at,__v,created_by,updated_by
+
+PRE_STMT="drop table if exists singletype1s;\
+create table singletype1s (\
+    id int(10) unsigned not null auto_increment,\
+    inhalt varchar(10000),\
+    primary key (id)\
+) engine=InnoDB default charset=utf8mb4 collate=utf8mb4_unicode_ci;"
+pull_collection "$PRE_STMT" singletype1s "_id,inhalt,createdAt,updatedAt,__v,created_by,updated_by" 
+
+# _id,username,registrationToken,firstname,lastname,email,password,created_at,updated_at,__v,resetPasswordToken,preferedLanguage
+# 5f25e7a5a0536c001712d60d,,,Tilo,Thiele,tilo.thiele@hamburg.de,$2a$10$XFn0vrMqrh5blfM6OcouFekuNqTMchfCMePUGCybAnGli4Q3N2LCm,2020-08-01,2020-08-01,0,,de
+
+PRE_STMT="drop table if exists strapi_administrator;\
+create table strapi_administrator (\
+    id int(10) unsigned not null auto_increment,\
+    inhalt varchar(10000),\
+    username varchar(50),\
+    firstname varchar(50),\
+    lastname varchar(50),\
+    email varchar(50),\
+    primary key (id)\
+) engine=InnoDB default charset=utf8mb4 collate=utf8mb4_unicode_ci;"
+pull_collection "$PRE_STMT" "strapi_administrator" "_id,username,firstname,lastname,email,createdAt,updatedAt,__v"
+
+PRE_STMT="drop table if exists termines;\
+create table termines (\
+    id int(10) unsigned not null auto_increment,\
+    inhalt varchar(10000),\
+    primary key (id)\
+) engine=InnoDB default charset=utf8mb4 collate=utf8mb4_unicode_ci;"
+pull_collection "$PRE_STMT" "termines" "_id,inhalt,createdAt,updatedAt,__v,created_by,updated_by"
+
+# _id,name,alternativeText,caption,hash,ext,mime,size,width,height,url,formats,provider,created_at,updated_at,created_by,updated_by,provider_metadata
+# 5f5e81d8ef03b600177d9274,portrait-thomasp-150x150.jpg,,,portrait_thomasp_150x150_10b0333fb2,.jpg,image/jpeg,4.44,150,150,https://res.cloudinary.com/dgibmteg8/image/upload/v1600029144/portrait_thomasp_150x150_10b0333fb2.jpg,,cloudinary,2020-09-13,2021-07-09,5f25e7a5a0536c001712d60d,5f25e7a5a0536c001712d60d,"{""public_id"":""portrait_thomasp_150x150_10b0333fb2"",""resource_type"":""image""}"
+PRE_STMT="drop table if exists upload_file;\
+create table upload_file (\
+    id int(10) unsigned not null auto_increment,\
+    name varchar(100),\
+    alternativeText varchar(100),\
+    caption varchar(100),\
+    ext varchar(10),\
+    mime varchar(100),\
+    size float,\
+    width int,\
+    height int,\
+    url varchar(500),\
+    formats varchar(200),\
+    provider varchar(100),\
+    provider_metadata varchar(1000),\
+    primary key (id)\
+) engine=InnoDB default charset=utf8mb4 collate=utf8mb4_unicode_ci;"
+pull_collection "$PRE_STMT" "upload_file" "_id,name,alternativeText,caption,ext,mime,size,width,height,url,formats,provider,createdAt,updatedAt,created_by,updated_by,provider_metadata"
+
+PRE_STMT="drop table if exists naechster_clubabends;\
+create table naechster_clubabends (\
+    id int(10) unsigned not null auto_increment,\
+    datum timestamp,\
+    inhalt varchar(10000),\
+    primary key (id)\
+) engine=InnoDB default charset=utf8mb4 collate=utf8mb4_unicode_ci;"
+pull_collection "$PRE_STMT" "naechster_clubabends" "_id,Datum,inhalt,createdAt,updatedAt,__v,created_by,updated_by"
+
+PRE_STMT="drop table if exists core_store;\
+create table core_store (\
+    id int(10) unsigned not null auto_increment,\
+    key_ varchar(100),\
+    environment varchar(100),\
+    tag varchar(100),\
+    value varchar(10000),\
+    type varchar(20),\
+    primary key (id)\
+) engine=InnoDB default charset=utf8mb4 collate=utf8mb4_unicode_ci;"
+pull_collection "$PRE_STMT" "core_store" "_id,key_,environment,tag,value,type"
 
 PRE_STMT="drop table if exists clubabende;\
 create table clubabende (\
@@ -88,18 +162,11 @@ create table clubabende (\
 ) engine=InnoDB default charset=utf8mb4 collate=utf8mb4_unicode_ci;"
 pull_collection "$PRE_STMT" "clubabende" "_id,titel,datum,inhalt,createdAt,updatedAt,__v,beitragsbild,created_by,updatedBy"
 
-#pull_collection "core_store" "_id,key,environment,tag,value,type,createdAt,updatedAt,__v"
-#pull_collection "naechster_clubabends" "_id,Datum,inhalt,createdAt,updatedAt,__v,created_by,updated_by"
-#pull_collection singletype1s --type=csv --fields "" $IMPORT_MONGO_DB_URI
-#pull_collection "strapi_administrator" "_id,isActive,blocked,username,registrationToken,firstname,lastname,email,password,createdAt,updatedAt,__v,resetPasswordToken,preferedLanguage"
 
 # wird nicht benötigt
 ##pull_collection "strapi_permission" "_id,action,subject,conditions,fields,createdAt,updatedAt,__v,role"
 ##pull_collection "strapi_role" "_id,name,code,description,createdAt,updatedAt,__v"
 ##pull_collection "strapi_webhooks" "_id,name,url,headers,events,enabled,createdAt,updatedAt,__v"
-
-#pull_collection "termines" "_id,inhalt,createdAt,updatedAt,__v,created_by,updated_by"
-#pull_collection "upload_file" "_id,name,alternativeText,caption,hash,ext,mime,size,width,height,url,formats,provider,createdAt,updatedAt,created_by,updated_by,provider_metadata"
 
 # wird nicht benötigt
 ##pull_collection users-permissions_permission --type=csv --fields "" $IMPORT_MONGO_DB_URI
